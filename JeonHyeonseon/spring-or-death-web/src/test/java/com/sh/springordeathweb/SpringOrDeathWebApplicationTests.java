c21a6a4d576ecd49fa53a6e31756919109118485
jeonhyeonseon <good_pretty@naver.com>
2024-07-09 18:20:02 +0900

    Feat: 환경설정

---
+++ JeonHyeonseon/spring-or-death-web/src/test/java/com/sh/springordeathweb/SpringOrDeathWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.springordeathweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrDeathWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
